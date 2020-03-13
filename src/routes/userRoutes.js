ecf7b930d8797cc8ab583ca838299c45fd6f6027
Oladehinde Olanrewaju Kazeem <larrysnet2001@gmail.com>
2020-03-13 01:05:07 +0100

    feat(delete user): delete a single user
    - create delete route
    - create delete function in controller
    
    [Finishes]

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -2,17 +2,16 @@ import userController from '../controllers/userController';
 import validator from '../middlewares/validator';
 
 const { validateCreate, validateUpdate } = validator;
-const { createUser, updateUser, getAll, getOne } = userController;
+const {
+  createUser, updateUser, getAll, getOne, deleteOne
+} = userController;
 
 const userRoutes = (router) => {
-  router.route('/users/create')
-    .post(validateCreate, createUser);
-  router.route('/users/update/:id')
-    .patch(validateUpdate, updateUser);
-  router.route('/users')
-    .get(getAll);
-  router.route('/users/:id')
-    .get(getOne);
+  router.route('/users/create').post(validateCreate, createUser);
+  router.route('/users/update/:id').patch(validateUpdate, updateUser);
+  router.route('/users').get(getAll);
+  router.route('/users/:id').get(getOne);
+  router.route('/users/:id').delete(deleteOne);
 };
 
 export default userRoutes;
